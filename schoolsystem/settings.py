79d6106fc308e6633af208619fdfce7898bda32f
ycarotrindade <ycarobanes@gmail.com>
2024-06-20 13:54:24 -0400

    adding logic for .env

--- schoolsystem/settings.py
+++ schoolsystem/settings.py
@@ -25,7 +25,7 @@ load_dotenv(os.path.join(BASE_DIR/".eVar",".env"))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = False if os.environ.get('DEBUG') == 'False' else True
 
 ALLOWED_HOSTS = ["127.0.0.1",'localhost']
 ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS").split()
